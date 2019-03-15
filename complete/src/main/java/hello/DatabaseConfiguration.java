e10ccde51420d2408955d89a021d413ac9d6da81
James Davidson <jamesdavidson@structmap.com>
2019-03-15 06:50:24 +0000

    Switch back to using HikariCP

--- complete/src/main/java/hello/DatabaseConfiguration.java
+++ complete/src/main/java/hello/DatabaseConfiguration.java
@@ -3,15 +3,16 @@ package hello;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
+import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.HikariConfig;
 
 @Configuration
 public class DatabaseConfiguration {
 
     @Bean
-    public MysqlDataSource dataSource() {
-        MysqlDataSource ds = new MysqlDataSource();
-        ds.setURL("jdbc:mysql://"
+    public HikariDataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://"
                   +System.getenv("MYSQL_HOSTNAME")+"/"
                   +System.getenv("MYSQL_DATABASE")
                   +"?useSSL=true"
@@ -21,8 +22,9 @@ public class DatabaseConfiguration {
                   +"&prepStmtCacheSize=250"
                   +"&prepStmtCacheSqlLimit=2048"
                   +"&useServerPrepStmts=true");
-        ds.setUser(System.getenv("MYSQL_USERNAME"));
-        ds.setPassword(System.getenv("MYSQL_PASSWORD"));
+        config.setUsername(System.getenv("MYSQL_USERNAME"));
+        config.setPassword(System.getenv("MYSQL_PASSWORD"));
+        HikariDataSource ds = new HikariDataSource(config);
         return ds;
     }
 }
