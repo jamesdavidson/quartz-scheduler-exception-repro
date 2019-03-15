01ba8358ff2f73db2093aebf84ea0357637f072d
James Davidson <jamesdavidson@structmap.com>
2019-03-15 03:59:49 +0000

    Change config format

--- complete/src/main/java/hello/DatabaseConfiguration.java
+++ complete/src/main/java/hello/DatabaseConfiguration.java
@@ -12,14 +12,17 @@ public class DatabaseConfiguration {
     @Bean
     public HikariDataSource dataSource() {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://"+System.getenv("MYSQL_HOSTNAME")+":3306/"+System.getenv("MYSQL_DATABASE"));
+        config.setJdbcUrl("jdbc:mysql://"
+                  +System.getenv("MYSQL_HOSTNAME")+"/"
+                  +System.getenv("MYSQL_DATABASE")
+                  +"?useSSL=true"
+                  +"&parseInfoCacheFactory=hello.PerConnectionLRUFactory"
+                  +"&cachePrepStmts=true"
+                  +"&prepStmtCacheSize=250"
+                  +"&prepStmtCacheSqlLimit=2048"
+                  +"&useServerPrepStmts=true");
         config.setUsername(System.getenv("MYSQL_USERNAME"));
         config.setPassword(System.getenv("MYSQL_PASSWORD"));
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("parseInfoCacheFactory", "hello.PerConnectionLRUFactory");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-        config.addDataSourceProperty("useServerPrepStmts", "true"); // false fixes problem ?!
         HikariDataSource ds = new HikariDataSource(config);
         return ds;
     }
